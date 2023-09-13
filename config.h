3380cbbc3b8565fcf7bf5d62f2f214d222a0a3d2
amfyoyo <randal.helm@outlook.com>
2023-09-13 00:14:31 -0700

    Add files via upload

---
+++ config.h
@@ -0,0 +1,151 @@
+/* See LICENSE file for copyright and license details. */
+
+/* appearance */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int snap      = 10;       /* snap pixel */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *fonts[]          = { "monospace:size=12" };
+static const char dmenufont[]       = "monospace:size=12";
+static const char col_gray1[]       = "#222222"; /*background-color*/
+static const char col_gray2[]       = "#444444"; /*inactive window border color*/
+static const char col_gray3[]       = "#bbbbbb"; /* font color */
+static const char col_gray4[]       = "#eeeeee"; /* Selected tag / window font color */
+static const char col_cyan[]        = "#444444"; /* Bar Highlight color / selected window border color (original) #005577 */
+static const char *colors[][3]      = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+};
+
+/* tagging */
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+static const Rule rules[] = {
+	/* xprop(1):
+	 *	WM_CLASS(STRING) = instance, class
+	 *	WM_NAME(STRING) = title
+	 */
+	/* class      instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "firefox",  NULL,       NULL,       1 << 8,       1,           -1 },
+        { "Signal",   NULL,       NULL,       1 << 7,       1,           -1 },
+        { "Todoist",   NULL,       NULL,       1 << 6,       1,           -1 },
+        { "thunderbird",   NULL,       NULL,   1 << 5,       1,           -1 },
+	{ "Joplin",   NULL,	  NULL,       1 << 4,	    1,		 -1 },
+	{ "Brave",   NULL,	  NULL,       1,	    1,		 -1 },
+};
+
+/* layout(s) */
+static const float mfact     = 0.65; /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;    /* number of clients in master area */
+static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+
+static const Layout layouts[] = {
+	/* symbol     arrange function */
+	{ "[t]",      tile },    /* first entry is default */
+	{ "[f]",      NULL },    /* no layout function means floating behavior */
+	{ "[m]",      monocle },
+};
+
+/* key definitions */
+#define MODKEY Mod1Mask
+#define TAGKEYS(KEY,TAG) \
+	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+
+/* helper for spawning shell commands in the pre dwm-5.0 fashion */
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+
+/* commands */
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]  = { "st", "-f", "Liberation Mono:pixelsize=18:antialias=true:autohint=true" };
+static const char *upvol[] = { "/home/randmin/.config/_keybind_scripts/raise_volume.sh", NULL };
+static const char *downvol[] = { "/home/randmin/.config/_keybind_scripts/lower_volume.sh", NULL };
+static const char *upbright[] = { "sudo", "/home/randmin/.config/_keybind_scripts/raise_brightness.sh" , NULL };
+static const char *downbright[] = { "sudo", "/home/randmin/.config/_keybind_scripts/lower_brightness.sh", NULL };
+static const char *suspend[] = { "/home/randmin/.config/_keybind_scripts/suspend_system.sh", NULL };
+static const char *refreshwallpaper[] = { "/home/randmin/.config/_keybind_scripts/refresh_wallpapers.sh", NULL };
+static const char *setmonitors[] = { "/home/randmin/.config/_keybind_scripts/set_monitors.sh", NULL }; 
+static const char *openjoplin[] = { "joplin-desktop", NULL };
+static const char *openfirefox[] = { "firefox", NULL };
+static const char *opensignal[] = { "signal-desktop", NULL };
+static const char *opentodoist[] = { "todoist-electron", NULL };
+static const char *openthunderbird[] = { "thunderbird", NULL };
+static const char *flipscreen[] = { "/home/randmin/.config/_keybind_scripts/invert_screen.sh", NULL };
+static const char *revertscreen[] = { "/home/randmin/.config/_keybind_scripts/revert_screen.sh", NULL };
+
+
+static Key keys[] = {
+	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	TAGKEYS(                        XK_1,                      0)
+	TAGKEYS(                        XK_2,                      1)
+	TAGKEYS(                        XK_3,                      2)
+	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_5,                      4)
+	TAGKEYS(                        XK_6,                      5)
+	TAGKEYS(                        XK_7,                      6)
+	TAGKEYS(                        XK_8,                      7)
+	TAGKEYS(                        XK_9,                      8)
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+  { 0,       0x1008ff11,  spawn, {.v = downvol } },
+  { 0,       0x1008ff13,  spawn, {.v = upvol } },
+  { 0,       XK_F6,       spawn, {.v = upbright } },
+  { 0,       XK_F5,       spawn, {.v = downbright } },
+	{ MODKEY|ShiftMask,	XK_z, 	  spawn, {.v = suspend } },
+	{ MODKEY|ShiftMask, 	XK_w,	  spawn, {.v = refreshwallpaper } },
+	{ MODKEY|ShiftMask,     XK_x,     spawn, {.v = setmonitors } },
+  { MODKEY|ShiftMask,     XK_j,	  spawn, {.v = openjoplin } },
+  { MODKEY|ShiftMask,     XK_f,     spawn, {.v = openfirefox } },
+  { MODKEY|ShiftMask,     XK_t,     spawn, {.v = opentodoist } },
+  { MODKEY,     		XK_s,     spawn, {.v = opensignal } },
+  { MODKEY|ShiftMask,	XK_e,	spawn, {.v = openthunderbird } },
+  { MODKEY|ShiftMask,	0xff52,	spawn, {.v = flipscreen } },
+  { MODKEY|ShiftMask,	0xff54,	spawn, {.v = revertscreen } },
+};
+
+/* button definitions */
+/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+static Button buttons[] = {
+	/* click                event mask      button          function        argument */
+	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
+	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
+	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkTagBar,            0,              Button1,        view,           {0} },
+	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+
+};
+
---
+++ dependencies/install_dependencies.sh
@@ -0,0 +1,1 @@
+sudo apt install build-essential libx11-dev libxinerama-dev sharutils libxft-dev
